1e53713e14a3e38f84ccb80edba99ed6d53620d1
VanyaMate <buhu653@mail.ru>
2024-07-25 16:40:22 +0300

    up temp Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Start()
-    {
-    }
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotateSpeed = 5f;
 
-    void Update()
+    private void Update()
     {
+        Vector3 inputVector = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.z += 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.z -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+        transform.position += Time.deltaTime * moveSpeed * inputVector;
+        transform.forward = Vector3.Slerp(transform.forward, inputVector, Time.deltaTime * rotateSpeed);
     }
 }
